f01a5389733753a6949abc118fe65e855a9544d0
Vitali Romanenko <romanenko.vit.kharkiv@gmail.com>
2021-12-11 16:27:11 +0200

    Ht18 wrote function anon/

--- ht19/Romanenko/control.php
+++ ht19/Romanenko/control.php
@@ -1,15 +1,19 @@
 <?php
 namespace Romanenko;
 class Control{
-    public function Filter($users){
-    $adult=18;
-    $users_new=[];
-        for ($i=0; $i<3; $i++){
-            if ($users[$i]->age > $adult) {
-                $users_new[]=$users[$i];
+    public function Filter($users)
+    {
+        $users_new = [];
+        for ($i = 0; $i < 3; $i++) {
+            function ($v){
+                $adult = 18;
+                if ($users[$i]->age > $adult) {
+                    $users_new[] = $users[$i];
+                }
             }
+
+            return $users_new;
         }
-       return $users_new;
     }
 }
 
