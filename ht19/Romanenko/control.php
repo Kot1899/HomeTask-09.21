88635f21a418da5ccd10658b6d9efb600680bc5d
Kot1899 <romanenko.vit.kharkiv@gmail.com>
2021-12-13 15:09:25 +0200

    hometask 19 corected of Sacha/

--- ht19/Romanenko/control.php
+++ ht19/Romanenko/control.php
@@ -1,18 +1,16 @@
 <?php
 namespace Romanenko;
-class Control{
-    public function Filter($users){
-    $adult=18;
-    $users_new=[];
-        for ($i=0; $i<3; $i++){
-            if ($users[$i]->age > $adult) {
-                $users_new[]=$users[$i];
+class Control
+{
+    public function Filter($users)
+    {
+        return array_filter($users, function ($v) {
+            if ($v->age > 18) {
+                return true;
+            } else {
+                return false;
             }
-        }
-       return $users_new;
+        });
     }
 }
 
-
-
-array_filter($users,function($v));
