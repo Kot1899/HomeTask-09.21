6b081855f9fb66f1fd0c8ef421d4026f8fa48876
Kot1899 <romanenko.vit.kharkiv@gmail.com>
2021-12-13 21:41:30 +0200

    hometask 21 four parts of task

--- ht21/index.php
+++ ht21/index.php
@@ -1,6 +1,27 @@
 <?php
 $connection = new PDO('mysql:host=localhost; dbname=myCat', 'root', "123123");
-//$connection->exec('insert into ht2 (name, age, city, number_car)
-// values ('ivan', 10, 'KH', 2), ('mike', 12, 'LA', 3)');
-$connection->exec('delete from ht2 where id=12');
+
+// first part of task
+
+//$connection->exec(  "insert into ht2 (name, age, city, number_car)
+// values ('mike', 14, 'KH', 5), ('iivan', 10, 'KH', 2)");
+
+
+// first part of task
+
+//$connection->exec(  "update ht2 set name='Rita', age=77 where name='iivan'");
+//echo 'oK';
+
+
+// third part of task
+//$connection->exec('delete from ht2 where name="mike" ');
+//echo 'oK';
+
+
+//fourth part of task
+
+$stm= $connection->query(" select * from ht2 ");
+$stm->execute();
+$list=$stm->fetchAll(PDO::FETCH_ASSOC);
+var_dump($list);
 echo 'oK';
