e90d024cce0623b11ed1db637d76acb8ef6a07b7
Kot1899 <romanenko.vit.kharkiv@gmail.com>
2022-01-04 17:34:37 +0200

    hometask 25 прописал switch  но ошибка NaN

--- ht25_js/js1.js
+++ ht25_js/js1.js
@@ -4,6 +4,10 @@ function clearbutton(){
     inputElement.value=''; //присвоим значение что равно пустоте
 }
 
+var numberCal;
+var symbolCal;
+var inputValue;
+var resultCal;
 
 //  первая кнопка для примера с числом
 // function calculator(){
@@ -16,49 +20,51 @@ function clearbutton(){
 
 //функция с аргументом (реализовал для кнопки 0-9)
 function calculateNumber(el){
+
+    // обнуляем сначала поле результата
+    let resultElement=document.getElementById('result');
+    document.getElementById('result').value='';
     let inputElement=document.getElementById('input');
     el.value;
     inputElement.value = inputElement.value+el.value;
-    let number = inputElement.value;  //переменная Б, просто числа
-    return number;
-}
+    numberCal = inputElement.value;  //переменная Б, просто числа
 
+}
 
 // метод для символов
 function calculateSymbol(elem){
-    let inputValue=document.getElementById('input').value; //inputValue - это перменная А с числом которую запоминаем
-    let symbol= elem.value; //el.value - это переменная ЗНАК МАТ-ОПЕРАЦИИ
-    let inputElement=document.getElementById('input');
+    document.getElementById('input').value=''; //обнуляем поле сначала
+    inputValue=document.getElementById('input').value; //inputValue - это перменная А с числом которую запоминаем
+    symbolCal= elem.value; //el.value - это переменная ЗНАК МАТ-ОПЕРАЦИИ
+    // let inputElement=document.getElementById('input');
     document.getElementById('input').value='';  //после выполнения расчета поле INPUT становится пустым
-    return inputValue;
-    return symbol;
+
 }
 
 
 //метод для выполнения функции РАВНО
 function equals(){
     // let equalsElement=document.getElementById('=')  //обращаемся к кнопке РАВНО
-    // let resultElement=document.getElementById('result');   // обратились к нопке РЕЗАЛТ
-    // let result; // некая перменная которая примет в себя результат логического вычесления
-    //
-    // switch (symbol){
-    //     case '+':
-    //         result=inputValue"+"number;
-    //         break;
-    //     case '-':
-    //         result=inputValue"-"number;
-    //         break;
-    //     case '*':
-    //         result=inputValue"*"number;
-    //         break;
-    //     case '/':
-    //         result=inputValue"/"number;
-    //         break;
-    //     default:
-    //         result='error';
-    // }
-    // resultElement.value= result;  //  в поле РЕЗУЛЬТАТА калькулятора выведется значение итоговое
-    //  document.getElementById('input').value='';  //после выполнения расчета поле INPUT становится пустым
+    let resultElement=document.getElementById('result');   // обратились к нопке РЕЗАЛТ
+
+    switch (symbolCal){
+        case '+':
+            resultCal=parseInt(inputValue)+parseInt(numberCal);
+            break;
+        case '-':
+            resultCal=parseInt(inputValue)-parseInt(numberCal);
+            break
+        case '*':
+            resultCal=parseInt(inputValue)*parseInt(numberCal);
+            break;
+        case '/':
+            resultCal=parseInt(inputValue)/parseInt(numberCal);
+            break;
+        default:
+            resultCal='error';
+    }
+    resultElement.value= resultCal;  //  в поле РЕЗУЛЬТАТА калькулятора выведется значение итоговое
+     document.getElementById('input').value='';  //после выполнения расчета поле INPUT становится пустым
 }
 
 
