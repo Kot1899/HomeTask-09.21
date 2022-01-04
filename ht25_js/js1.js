fbfbf58c0f95d78e71ccd3ecc8bfc2d41fa13bdc
Kot1899 <romanenko.vit.kharkiv@gmail.com>
2022-01-04 10:44:15 +0200

    hometask 25 кнопку 7 добавлять в инпут смог

--- ht25_js/js1.js
+++ ht25_js/js1.js
@@ -2,10 +2,18 @@
 
 function calculator(){
     let inputElement=document.getElementById('input');// переменная для поля ввода
-
     let sevenElement=document.getElementById('7'); // переменная для button 7
-    let sevenVal= 7;   // присвоили в переменную значение что будем вводить
-    inputElement.innerText=inputElement+sevenVal;   // в кнопку переименовуем на значения из инпута
+    let sevenVal= '7';   // присвоили в переменную значение что будем вводить
+    // let eightVal= '8';
+    // let nineVal= '9';
+    // let fourVal= '4';
+    // let fiveVal= '5';
+    // let sixVal= '6';
+    // let oneVal= '1';
+    // let twoVal= '2';
+    // let threeVal= '3';
+    inputElement.value = inputElement.value+sevenVal;
+        // +eightVal+nineVal+fourVal+fiveVal+sixVal+oneVal+twoVal+threeVal;
 }
 
 // кнопка сброса ввода =РАБОТАЕТ
@@ -13,3 +21,6 @@ function clearbutton(){
     let inputElement=document.getElementById('input');
     inputElement.value=''; //присвоим значение что равно пустоте
 }
+
+
+
