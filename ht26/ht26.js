75eb8697a6aae962ca23bf01a4119d844022ee24
Vitali Romanenko <romanenko.vit.kharkiv@gmail.com>
2022-01-10 15:51:31 +0200

    Ht26 created in ht26.js 4 colorful square

--- ht26/ht26.js
+++ ht26/ht26.js
@@ -18,12 +18,79 @@ document.documentElement.clientWidth
 
 document.getElementById('body').addEventListener('mousemove',
     function (e){
-    console.log(e);
+    // console.log(e);
     moveObj(e)
 });
 
 function moveObj(e){
     let test= document.getElementById('square');
-    test.style.top= (e.clientY)+'px';
-    test.style.left= (e.clientX)+'px';
+    let Y_axis = test.style.top= (e.clientY)+'px';
+    let X_axis =test.style.left= (e.clientX)+'px';
+
+    // попробуем просто вывести перменные с осями в консоль
+    console.log(Y_axis);
+    console.log(X_axis);
+
+    // 1/4 четверть
+    re1_y=400+'px';
+    re1_x=720+'px';
+
+    let re1=document.getElementById('rectangle1');
+    if ((Y_axis<re1_y) && (X_axis<re1_x)){
+        re1.style.backgroundColor = 'blue';
+        }else{
+        re1.style.backgroundColor = 'black';
+    }
+
+    // 2/4 четверть
+     let re2=document.getElementById('rectangle2');
+    if ((Y_axis<re1_y) &&  (X_axis>re1_x)){
+        re2.style.backgroundColor = '#F07C5B';
+    }else{
+        re2.style.backgroundColor = 'black';
+    }
+
+    // 3/4 четверть
+    let re3=document.getElementById('rectangle3');
+    if ((Y_axis>re1_y) &&  (X_axis<re1_x)){
+        re3.style.backgroundColor = '#4FF88F';
+    }else{
+        re3.style.backgroundColor = 'black';
+    }
+
+    // 4/4 четверть
+    let re4=document.getElementById('rectangle4');
+    if ((Y_axis>re1_y) &&  (X_axis>re1_x)){
+        re4.style.backgroundColor = '#FAE71D';
+    }else{
+        re4.style.backgroundColor = 'black';
+    }
 }
+/*
+// let a=1;
+// 1/4 четверть
+// let re1=document.getElementById('rectangle1');
+// if (a>4){
+//     re1.style.backgroundColor = 'blue';
+//     // re1.innerHTML='<style>#rectangle1 { background-color:#F07C5B;} </style>';  все равно не сработал
+// }
+
+// 2/4 четверть
+// let re2=document.getElementById('rectangle2');
+// if (a>4){
+//     re2.style.backgroundColor = '#F07C5B';
+// }
+
+// 3/4 четверть
+// let re3=document.getElementById('rectangle3');
+// if (a>4){
+//     re3.style.backgroundColor = '#4FF88F';
+// }
+
+// 4/4 четверть
+// let re4=document.getElementById('rectangle4');
+// if (a>4){
+//     re4.style.backgroundColor = '#FAE71D';
+// }
+
+ */
