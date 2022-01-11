c20f84bf509e427bf0f427c9ecf3f8cb5ac560d8
Vitali Romanenko <romanenko.vit.kharkiv@gmail.com>
2022-01-11 09:46:39 +0200

    Ht26 created in ht26.js 4 colorful square without function!

--- ht26/ht26.js
+++ ht26/ht26.js
@@ -33,8 +33,44 @@ function moveObj(e){
     // попробуем просто вывести перменные с осями в консоль
     console.log(Y_axis);
     console.log(X_axis);
+
+    // 1/4 четверть
+    re1_y=400+'px';
+    re1_x=720+'px';
+    let re1=document.getElementById('rectangle1');
+    if ((Y_axis<re1_y) && (X_axis<re1_x)){
+        re1.style.backgroundColor = 'blue';
+    }else{
+        re1.style.backgroundColor = 'black';
+    }
+
+// 2/4 четверть
+    let re2=document.getElementById('rectangle2');
+    if ((Y_axis<re1_y) &&  (X_axis>re1_x)){
+        re2.style.backgroundColor = '#F07C5B';
+    }else{
+        re2.style.backgroundColor = 'black';
+    }
+
+// 3/4 четверть
+    let re3=document.getElementById('rectangle3');
+    if ((Y_axis>re1_y) &&  (X_axis<re1_x)){
+        re3.style.backgroundColor = '#4FF88F';
+    }else{
+        re3.style.backgroundColor = 'black';
+    }
+
+// 4/4 четверть
+    let re4=document.getElementById('rectangle4');
+    if ((Y_axis>re1_y) &&  (X_axis>re1_x)){
+        re4.style.backgroundColor = '#FAE71D';
+    }else{
+        re4.style.backgroundColor = 'black';
+    }
 }
 
+
+/*
 // 1/4 четверть
 re1_y=400+'px';
 re1_x=720+'px';
@@ -68,11 +104,7 @@ if ((Y_axis>re1_y) &&  (X_axis>re1_x)){
 }else{
     re4.style.backgroundColor = 'black';
 }
-
-
-
-
-
+*/
 
 /*
 // let a=1;
