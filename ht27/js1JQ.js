87c6a3f8d7a19ca4f76da127b8545c4c26eb852d
Kot1899 <romanenko.vit.kharkiv@gmail.com>
2022-01-14 17:03:44 +0200

    hometask 27 made button %

--- ht27/js1JQ.js
+++ ht27/js1JQ.js
@@ -1,89 +1,77 @@
-
-
 // кнопка сброса ввода =РАБОТАЕТ
 function clearbutton(){
-    let inputElement=document.getElementById('input');
-    inputElement.value=''; //присвоим значение что равно пустоте
+    $('#input').val('');
 }
 
+//создаем глобальные переменные
 var numberCal;
 var symbolCal;
 var inputValue;
 var resultCal;
-
-
-
-//LISTEN CLICK EVENT
-var buttons=document.getElementById('btn');
-buttons.addEventListener('click', function (e){
-    alert("click me");
-});
-
-$('#btn').on('click',function (e) {
-    alert('click with jquery')
-});
-
+var proc=100;
 
 //функция с аргументом (реализовал для кнопки 0-9)
-function calculateNumber(el){
-
-    // обнуляем сначала поле результата
-
-    // let resultElement=document.getElementById('result');
-    // document.getElementById('result').value='';
-    // let inputElement=document.getElementById('input');
-    // inputElement.value = inputElement.value+el.innerHTML;
-    // numberCal = inputElement.value;  //переменная Б, просто числа
-
-    $('#result').val('');
-    $('#input').val('');
-
+/*
+function calculateNumber(el){//
+   // обнуляем сначала поле результата
+   let resultElement=document.getElementById('result');
+   document.getElementById('result').value='';
+   let inputElement=document.getElementById('input');
+   inputElement.value = inputElement.value+el.innerHTML;
+   numberCal = inputElement.value;  //переменная Б, просто числа
+}*/
+function calculateNumber(el) {
+    $('#result').val('');    // обнуляем сначала поле результата
+    $('#input').val($('#input').val() + $(el).html());
+    numberCal = $('#input').val();  //переменная Б, просто числа
 }
 
 // метод для символов
-function calculateSymbol(elem){
+      function calculateSymbol(elem) {
+        inputValue = $('#input').val();   //inputValue - это перменная А с числом которую запоминаем
+        $('#input').val('');             //обнуляем поле сначала
+        symbolCal = $(elem).html();      // - это переменная ЗНАК МАТ-ОПЕРАЦИИ
+        $('#input').val('');             //после выполнения расчета поле INPUT становится пустым
+    }
 
-    inputValue=document.getElementById('input').value; //inputValue - это перменная А с числом которую запоминаем
+// метод для M-
+function calculateSymbolM(elem) {
 
-    document.getElementById('input').value=''; //обнуляем поле сначала
-
-    symbolCal= elem.innerHTML; // - это переменная ЗНАК МАТ-ОПЕРАЦИИ
-
-    document.getElementById('input').value='';  //после выполнения расчета поле INPUT становится пустым
-
+    $('#input').val('');             //после выполнения расчета поле INPUT становится пустым
+    $('#result').val('bonjour ma petite souris').css({
+        fontSize: '18px',
+        color:'blue'
+    });
 }
 
+// метод для %
+function calculateSymbolPer1(per) {
+    inputValue = $('#input').val();   //inputValue - это перменная А с числом которую запоминаем
+    $('#result').val(   $('#input').val() / parseInt(proc) );
+    $('#input').val('');             //обнуляем поле сначала
+}
 
 //метод для выполнения функции РАВНО
-function equals(){
+    function equals() {
+        if ($('#result').val() != null) {
+            switch (symbolCal) {
+                case '+':
+                    resultCal = parseInt(inputValue) + parseInt(numberCal);
+                    break;
+                case '-':
+                    resultCal = parseInt(inputValue) - parseInt(numberCal);
+                    break
+                case '*':
+                    resultCal = parseInt(inputValue) * parseInt(numberCal);
+                    break;
+                case '/':
+                    resultCal = parseInt(inputValue) / parseInt(numberCal);
+                    break;
+                default:
+                    resultCal = 'error';
+            }
+            $('#result').val(resultCal);    //  в поле РЕЗУЛЬТАТА калькулятора выведется значение итоговое
+            $('#input').val('');         //после выполнения расчета поле INPUT становится пустым
 
-     let resultElement=document.getElementById('result');   // обратились к нопке РЕЗАЛТ
-
-    switch (symbolCal){
-        case '+':
-            resultCal=parseInt(inputValue)+parseInt(numberCal);
-            break;
-        case '-':
-            resultCal=parseInt(inputValue)-parseInt(numberCal);
-            break
-        case '*':
-            resultCal=parseInt(inputValue)*parseInt(numberCal);
-            break;
-        case '/':
-            resultCal=parseInt(inputValue)/parseInt(numberCal);
-            break;
-        default:
-            resultCal='error';
+              }
     }
-    resultElement.value= resultCal;  //  в поле РЕЗУЛЬТАТА калькулятора выведется значение итоговое
-     document.getElementById('input').value='';  //после выполнения расчета поле INPUT становится пустым
-}
-
-
-
-
-
-
-https://github.com/Kot1899/HomeTask-09.21.git
-    https://github.com/Kot1899/HomeTask-09.21.git
-        4eG2cJ:zy32P(L
