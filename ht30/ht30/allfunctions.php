c10ccf4e1de23bb6824d370816a59ac80c493fc8
Kot1899 <romanenko.vit.kharkiv@gmail.com>
2022-01-23 17:47:32 +0200

    hometask 30 add md5 for password

---
+++ ht30/ht30/allfunctions.php
@@ -0,0 +1,62 @@
+<?php
+//function Count_index(){
+//    for ($i=0; $i<9; $i++){
+//    }
+//    return $i;
+//}
+//function First_expl ()
+//{
+//    foreach ($array_people as $v) {
+//        $usersFinish = explode(';', $v);
+//        $users[] = $usersFinish;
+//    }
+//    return $users;
+//}
+//
+//function For_show($name, $age){
+//    echo $name . ' = ' . $age . ' years' . "\n";
+//}
+//
+//function Header1($line1){
+//    echo $line1 . "\n";
+//}
+//
+//function table_users($name, $age){
+//    echo sprintf('|%6s|%4d |', $name, $age);
+//    echo "\n";
+//}
+//
+//function table_users7($name, $last_n, $age, $sex){
+//    $pr=sprintf('|%11s|%13s|%4s|%7s|', $name, $last_n, $age, $sex);
+//    return $pr;
+//}
+//
+//function table_users8($name, $last_n, $age, $sex){
+//    $pr=sprintf('|%11s|%13s|%9s|%7s|', $name, $last_n, $age, $sex);
+//    return $pr;
+//}
+//
+//function table_users9($name, $last_n, $age, $sex){
+//    $pr=sprintf('%11s%13s%9s%7s', $name, $last_n, $age, $sex);
+//    return $pr;
+//}
+function first_str()
+{
+$s='<tr>'.
+    '<td><strong><big><center>' . '#' . '</center></big></strong></td>'.
+    '<td><strong><big><center>' . 'Name' . '</center></big></strong></td>'.
+    '<td><strong><big><center>' . 'Age' . '</center></big></strong></td>'.
+    '<td><strong><big><center>' . 'Sex' . '</center></big></strong></td>'.
+    '<td><strong><big><center>' . 'Number' . '</center></big></strong></td>'.
+    '<td><strong><big><center>' . 'Serial' . '</center></big></strong></td>'.
+    '<td><strong><big><center>' . 'Date of issure' . '</center></big></strong></td>'.
+    '</tr>';
+       return $s;
+}
+//
+//echo '<tr>';
+//echo '<td><strong><big><center>' . '#' . '</center></big></strong></td>';
+//echo '<td><strong><big><center>' . 'Name' . '</center></big></strong></td>';
+//echo '<td><strong><big><center>' . 'Age' . '</center></big></strong></td>';
+//echo '<td><strong><big><center>' . 'Sex' . '</center></big></strong></td>';
+//echo '</tr>';
---
+++ ht30/ht30/index.php
@@ -0,0 +1,50 @@
+<?php
+
+echo base64_decode('0JLQvtC30YzQvNC4INC30LAg0L7RgdC90L7QstGDINGB0LLQvtGOINGA0LDQsdC+0YLRgyDQv9C+INC30LDQtNCw0L3QuNGOINGBINCw0LLRgtC+0YDQuNC30LDRhtC40LXQuSwg0YHQtdGB0YHQuNGP0LzQuCDQuCDQutGD0LrQuC4g0KPRgdC+0LLQtdGA0YjQtdC90YHRgtCy0YPQuSDQv9GA0L7RhtC10YHRgSDQsNCy0YLQvtGA0LjQt9Cw0YbQuNC4INGC0LDQutC40Lwg0L7QsdGA0LDQt9C+0LwsINGH0YLQvtCx0Ysg0LjRgdC/0L7Qu9GM0LfQvtCy0LDQu9GB0Y8g0LzQtdGC0L7QtCDRhdC10YjQuNGA0L7QstCw0L3QuNGPIG1kNSDQuNC70Lggc2hhMSDQv9GA0Lgg0L/RgNC+0LLQtdGA0LrQtSDQv9Cw0YDQvtC70Y8u');
+
+/*
+ * Возьми за основу свою работу по заданию с авторизацией, сессиями и куки. Усовершенствуй процесс авто
+ризации таким образом, чтобы использовался метод хеширования md5 или sha1 при проверке пароля.
+
+ */
+
+// №1 подключится к БД
+$connection = new PDO('mysql:host=localhost; dbname=ht23', 'root', "123123");
+
+$connection->exec(  "insert into regist_hash ( login, password_hash)  values ('root', md5('123123') ), ('admin', md5('admin'))");
+
+echo 'ok';
+
+SESSION_START();
+
+// №2 подключится к БД
+
+$login= $_POST['Login'];
+$password= md5($_POST['Passw']);
+
+// №2 проверка с БД
+
+//if ($user->password==md5($_POST['password'])){
+//
+//}
+
+$sgl="SELECT * FROM regist_hash where login='" . $login . "' and password='" . $password . "'";
+$stm= $connection->query($sgl);
+$stm->execute();
+$list=$stm->fetchAll(PDO::FETCH_ASSOC);
+
+if ($list!=null){
+    $reg=true;
+    $_session['register']=$reg;
+} else{
+    $reg=false;
+    $_session['register']=$reg;
+}
+
+// №3 направление на след страничку
+if($_session['register']==true){
+    header("Location: index24.html");
+
+} else{
+    header("Location: fault.html");
+}
