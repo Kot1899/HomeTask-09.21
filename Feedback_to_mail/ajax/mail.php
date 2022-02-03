92ff4fe1e1e09fb825f0c91b6badeb5d967a18b7
Kot1899 <romanenko.vit.kharkiv@gmail.com>
2022-02-03 13:43:08 +0200

    Feed back send to mail

---
+++ Feedback_to_mail/ajax/mail.php
@@ -0,0 +1,20 @@
+<?php
+//echo 'hello';
+
+$email=$_POST['email'];
+$name=$_POST['name'];
+$phone=$_POST['phone'];
+$message=$_POST['message'];
+
+$subject="=?utf-8?B?".base64_encode('message from site')."?=";//особая конструкция надо запомнить, а дальше просто зашифровано фраза
+$headers= "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
+
+$success=mail("romanenko.vit.kharkiv@gmail.com",$subject, $message,  $headers);//почта, куда отправится;тема письма; само сообщение; заголовки
+echo $success;
+
+
+
+
+
+
+?>
--- Feedback_to_mail/js/form.js
+++ Feedback_to_mail/js/form.js
@@ -14,23 +14,28 @@ $("#send_mail").on("click", function () {
     } else if (phone == "") {
         $('#error_mess').text('Enter phone');
         return false;
-    } else if (message.length <5 ) {
+    } else if (message.length < 5) {
         $('#error_mess').text('Enter message');
         return false;
     }
     $('#error_mess').text('');
 
+
     $.ajax({
         url: 'ajax/mail.php',
         type: 'POST',
-        cache: false,
-        data: { 'name': name, 'email': email, 'phone': phone, 'message': message}
+        cache: false, //кеширование
+        data: { 'name': name, 'email': email, 'phone': phone, 'message': message},
         dataType: 'html',
         beforeSend: function(){
             $("#send_mail").prop('disabled', true);   //заблокировать кнопку чтобы ее не можно было нажать много раз
         },
-        success: function (data2) {
-            alert(data2);
+        success: function (data) {
+            if(!data)   //означает если data= false
+                alert('was some errors');
+            else
+                $('#mail_form').trigger('reset');//эта конструкция очистит форму от всех
+
             $("#send_mail").prop('disabled', false);
         }
         });
